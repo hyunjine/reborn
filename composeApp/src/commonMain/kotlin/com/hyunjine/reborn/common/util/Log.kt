07a2b035ecf6428204a1f97f6d65333f3d22253d
hyunjine <thevlakk1@gmail.com>
2026-03-16 15:50:08 +0900

    feat: tagLog 함수 추가

--- composeApp/src/commonMain/kotlin/com/hyunjine/reborn/common/util/Log.kt
+++ composeApp/src/commonMain/kotlin/com/hyunjine/reborn/common/util/Log.kt
@@ -2,6 +2,10 @@ package com.hyunjine.reborn.common.util
 
 import io.github.aakira.napier.Napier
 
+fun tagLog(tag: String, vararg message: Any?) {
+    Napier.d(message.joinToString("\n"), tag = tag)
+}
+
 fun log(vararg message: Any?) {
     Napier.d(message.joinToString("\n"), tag = "winter")
 }
